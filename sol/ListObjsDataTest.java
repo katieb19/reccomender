8903b78d89ec14aba25b464ebc0f9106a2f7b08d
Valerie Aguilar Dellisanti <valerieaguilardellisanti@valeries-air.devices.brown.edu>
2021-02-23 12:44:10 -0500

    all

--- sol/ListObjsDataTest.java
+++ sol/ListObjsDataTest.java
@@ -10,8 +10,15 @@ public class ListObjsDataTest {
         attribute.add("gender");
         attribute.add("leadershipExperience");
         attribute.add("lastPositionDuration");
-        Candidate c = new Candidate("women", '','','');
-        Candidate c1 = new Candidate("male", '','','');
+        attribute.add("numWorkExperiences");
+        attribute.add("programmingLanguages");
+        attribute.add("gpa");
+        attribute.add("location");
+        attribute.add("hired");
+        Candidate c = new Candidate("women", true, "16 Months",
+                "4", "Python", "3.9", "New York", true);
+        Candidate c1 = new Candidate("male", true, "2 Months",
+                "1", "Python, Scala", "3.8", "New York", false);
         LinkedList<Candidate> candidates = new LinkedList<>();
         candidates.add(c);
         candidates.add(c1);
@@ -20,5 +27,23 @@ public class ListObjsDataTest {
         return list;
     }
 
-    public
+    public LinkedList<String> testGetAttributes(t Tester){
+        setup();
+
+        //Empty attribute list
+        LinkedList<String> emptyList = new LinkedList<String>();
+        candidates.removeAttr("gender");
+        attribute.removeAttr("leadershipExperience");
+        attribute.removeAttr("lastPositionDuration");
+        attribute.removeAttr("numWorkExperiences");
+        attribute.removeAttr("programmingLanguages");
+        attribute.removeAttr("gpa");
+        attribute.removeAttr("location");
+        attribute.removeAttr("hired");
+        t.checkExpect(candidates.attribute.equals(emptyList));
+
+        //Multiple elements list
+        t.checkExpect(candidates.attribute.equals(LinkedList<String>("gender", "leadershipExperience", "lastPositionDuration",
+                "numWorkExperiences", "programmingLanguages", "gpa", "location", "hired")));
+    }
 }
