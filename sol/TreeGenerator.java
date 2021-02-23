5fe645a03ccd9d51aabdb0fb5dbf56ef0b65f05e
Valerie Aguilar Dellisanti <valerieaguilardellisanti@valeries-air.devices.brown.edu>
2021-02-23 11:29:55 -0500

    all

--- sol/TreeGenerator.java
+++ sol/TreeGenerator.java
@@ -5,6 +5,8 @@ import src.IAttributeDatum;
 import src.IGenerator;
 import src.INode;
 
+import java.util.LinkedList;
+
 /*
  * Class for creating and interacting with a decision tree given a dataset.
  *
@@ -26,11 +28,39 @@ public class TreeGenerator<T extends IAttributeDatum> implements IGenerator {
     // build a decision tree to predict the named attribute
     @Override
     public INode buildClassifier(String targetAttr) {
-        if (this.data.node == null){
-            return;
+        //if it's empty
+        //pick first attribute - random  - create node that contains attribute
+
+        //Creating first node with attribute
+        Node firstNode = new Node();
+        firstNode.attribute = targetAttr;
+        firstNode.edges = null;
+
+        //Partition data
+        LinkedList<IAttributeDataset<T>> partitionedData = this.data.partition(targetAttr);
+        LinkedList<T> distinctData = this.data.distinct(targetAttr);
+
+        //Create Edges
+        Edge firstEdges = new Edge();
+
+        for ()
+
+            firstEdges.value = null;
+
+        if (!firstEdges.decision == FinalDecision){
+
         }
-        if (same value// do this)
-        if (not)
+
+
+        firstEdges.decision = null;
+        String value;
+        INode decision; //node or another class with an object (int, string)
+
+
+        //create edges: set value and decision can be the rest of the tree on the inner list
+        //condition to check if it's final decision or rest of tree
+        // recursive
+
     }
 
     // produce the decision predicted for the given datum
